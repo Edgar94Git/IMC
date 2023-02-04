2ce8cbcda88761158556635034756d83300ef724
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-02-04 17:09:03 -0600

    cambios
    cambios para calcular y cambiar el fondo del cardview

--- app/src/main/java/com/ereyes/imc/MainActivity.kt
+++ app/src/main/java/com/ereyes/imc/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.ereyes.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.ereyes.imc.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private var weight = 50
-    private var height = 150
+    private var weight = 60
+    private var height = 120f
     private var age = 28
+    private var genre: eTypeGenre = eTypeGenre.NOT_ASSIGNED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +35,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpSlider(){
-
+        mBinding.sliderHeight.addOnChangeListener { slider, value, fromUser ->
+            height = value
+            mBinding.tvHeightNumber.text = height.toString()
+        }
     }
 
     private fun setUpButton() {
@@ -51,17 +59,49 @@ class MainActivity : AppCompatActivity() {
                 age -= 1
                 tvAge.text = age.toString()
             }
+            btnCalculate.setOnClickListener {
+                val imc = calculateIMC()
+                openResultIntent(imc)
+            }
         }
+    }
+
+    private fun openResultIntent(imc: Double) {
+        val intent = Intent(this, ResultImcActivity::class.java)
+        intent.putExtra("RESULT_IMC", imc)
+        startActivity(intent)
+    }
+
+    private fun calculateIMC(): Double {
+        val df = DecimalFormat("#.##")
+        val imc = weight / ((height.toDouble()/100) * (height.toDouble()/100))
+        return df.format(imc).toDouble()
     }
 
     private fun setUpCardView(){
         with(mBinding){
             cardMale.setOnClickListener {
-                Snackbar.make(mBinding.root, "Hombre", Snackbar.LENGTH_LONG).show()
+                setBackGroundColor(eTypeGenre.MALE)
             }
             cardFemale.setOnClickListener {
-                Snackbar.make(mBinding.root, "Mujer", Snackbar.LENGTH_LONG).show()
+                setBackGroundColor(eTypeGenre.FEMALE)
             }
         }
+    }
+
+    private fun setBackGroundColor(typeGenre: eTypeGenre){
+        genre = typeGenre
+        mBinding.cardMale.setCardBackgroundColor(getCardBackgroundColor(typeGenre == eTypeGenre.MALE))
+        mBinding.cardFemale.setCardBackgroundColor(getCardBackgroundColor(typeGenre == eTypeGenre.FEMALE))
+    }
+
+    private fun getCardBackgroundColor(selected: Boolean): Int{
+        val realColor = if(selected){
+            R.color.background_fab
+        } else {
+            R.color.black
+        }
+
+        return ContextCompat.getColor(this, realColor)
     }
 }
