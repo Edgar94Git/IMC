472569670e57f80290e756f4d5acf690f7c95b44
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-02-04 18:01:40 -0600

    object constants

---
+++ app/src/main/java/com/ereyes/imc/Constants.kt
@@ -0,0 +1,11 @@
+package com.ereyes.imc
+
+/****
+ * Project: IMC
+ * From: com.ereyes.imc
+ * Created by Edgar Reyes Gonzalez on 2/4/2023 at 5:59 PM
+ * All rights reserved 2023.
+ ****/
+object Constants {
+    const val RESULT_IMC = "Result_IMC"
+}
