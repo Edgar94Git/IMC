c51ff1f1e3aee8afb1290d09b89d459f17058dfd
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-03-16 08:59:20 -0600

    colors
    se agregan color para el resultado text

--- app/src/main/java/com/ereyes/imc/ResultImcActivity.kt
+++ app/src/main/java/com/ereyes/imc/ResultImcActivity.kt
@@ -3,6 +3,7 @@ package com.ereyes.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import com.ereyes.imc.databinding.ActivityResultImcBinding
 
 class ResultImcActivity : AppCompatActivity() {
@@ -31,26 +32,27 @@ class ResultImcActivity : AppCompatActivity() {
     private fun initUI(result: Double) {
         when(result){
             in 0.00..18.50 -> {
-                dataUI(result, R.string.bajo_peso, R.string.bajo_peso_description)
+                dataUI(result, R.string.bajo_peso, R.string.bajo_peso_description, R.color.peso_bajo)
             }
             in 18.51..24.99 -> {
-                dataUI(result, R.string.normal_peso, R.string.normal_peso_description)
+                dataUI(result, R.string.normal_peso, R.string.normal_peso_description, R.color.peso_normal)
             }
             in 25.00..29.99 -> {
-                dataUI(result, R.string.sobrepeso, R.string.sobrepreso_description)
+                dataUI(result, R.string.sobrepeso, R.string.sobrepreso_description, R.color.peso_sobrepeso)
             }
             in 30.00..99.00 -> {
-                dataUI(result, R.string.obesidad, R.string.obesidad_description)
+                dataUI(result, R.string.obesidad, R.string.obesidad_description, R.color.obesidad)
             }
             else -> {
-                dataUI(0.0, R.string.text_error, R.string.text_error)
+                dataUI(0.0, R.string.text_error, R.string.text_error, R.color.obesidad)
             }
         }
     }
 
-    private fun dataUI(result: Double, resultIMCText: Int, description: Int){
+    private fun dataUI(result: Double, resultIMCText: Int, description: Int, color: Int){
         mBinding.tvResultIMC.text = result.toString()
         mBinding.tvResultIMCText.text = getString(resultIMCText)
+        mBinding.tvResultIMCText.setTextColor(ContextCompat.getColor(this, color))
         mBinding.tvResultDescription.text = getString(description)
     }
 
