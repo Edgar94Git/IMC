18c04eda18a11e175548c4596ba9764e486ebbf2
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-03-16 08:50:21 -0600

    mostrar resultado descripcion y titulo

--- app/src/main/java/com/ereyes/imc/ResultImcActivity.kt
+++ app/src/main/java/com/ereyes/imc/ResultImcActivity.kt
@@ -1,11 +1,8 @@
 package com.ereyes.imc
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.MenuItem
-import android.view.View
 import com.ereyes.imc.databinding.ActivityResultImcBinding
 
 class ResultImcActivity : AppCompatActivity() {
@@ -28,7 +25,33 @@ class ResultImcActivity : AppCompatActivity() {
 
     private fun getIntentExtras(){
         val result = intent.extras?.getDouble(Constants.RESULT_IMC) ?: -1.0
+        initUI(result)
+    }
+
+    private fun initUI(result: Double) {
+        when(result){
+            in 0.00..18.50 -> {
+                dataUI(result, R.string.bajo_peso, R.string.bajo_peso_description)
+            }
+            in 18.51..24.99 -> {
+                dataUI(result, R.string.normal_peso, R.string.normal_peso_description)
+            }
+            in 25.00..29.99 -> {
+                dataUI(result, R.string.sobrepeso, R.string.sobrepreso_description)
+            }
+            in 30.00..99.00 -> {
+                dataUI(result, R.string.obesidad, R.string.obesidad_description)
+            }
+            else -> {
+                dataUI(0.0, R.string.text_error, R.string.text_error)
+            }
+        }
+    }
+
+    private fun dataUI(result: Double, resultIMCText: Int, description: Int){
         mBinding.tvResultIMC.text = result.toString()
+        mBinding.tvResultIMCText.text = getString(resultIMCText)
+        mBinding.tvResultDescription.text = getString(description)
     }
 
     private fun setUpButton(){
